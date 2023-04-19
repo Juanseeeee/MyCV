e4c2117fc2f508cc66e8e61685c73976bf8083dc
Juanseeeee <118040039+Juanseeeee@users.noreply.github.com>
2023-04-19 00:42:22 -0300

    Original JS y CSS

---
+++ functions.js
@@ -0,0 +1,35 @@
+// Selecciona todos los enlaces del menú
+const links = document.querySelectorAll('nav a');
+
+// Añade un evento de clic a cada enlace
+links.forEach(link => {
+  link.addEventListener('click', smoothScroll);
+});
+
+// Función para desplazamiento suave
+function smoothScroll(event) {
+  event.preventDefault();
+
+  const targetId = this.getAttribute('href');
+  const targetPosition = document.querySelector(targetId).offsetTop;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 1000;
+  let start = null;
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+
+  function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t + 2) + b;
+  }
+
+  window.requestAnimationFrame(step);
+}
